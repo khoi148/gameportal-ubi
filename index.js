bc29938b3a082f1a387766019bf26f4108845516
khoi148 <khoiminhphan@yahoo.com>
2020-07-29 21:30:38 +0700

    changed port to env var

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = require("./app");
-const port = 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `Example app listening at http://localhost:${process.env.PORT || 3000}`
+  )
 );
