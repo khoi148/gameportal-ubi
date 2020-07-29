6c1041116b89379a1ae255da848862797d48c8f8
khoi148 <khoiminhphan@yahoo.com>
2020-07-30 00:03:42 +0700

    Added Mongoose to project, using ORM instead of pure Mongo driver

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+//Popular ORM Handler package that builds on top of MongoDB
+const mongoose = require("mongoose");
+//Package to use .env file as environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+//connection to mongoDB, using mongoose ORM
+const connectionString = process.env.DB_CONNECTION;
+mongoose.connect(connectionString, {
+  // some options to deal with deprecated warning, you don't have to worry about them.
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 //main routes here
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const gameinfoRouter = require("./routes/gameinfo");
-const playerinfoRouter = require("./routes/playerinfo");
+const gameinfoRouter = require("./routes/gameInfo");
+const playerinfoRouter = require("./routes/playerInfo");
 const rewardsRouter = require("./routes/rewards");
 const app = express();
 
--- routes/gameinfo.js
+++ routes/gameinfo.js
@@ -1,5 +1,21 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const GameInfo = require("../models/gameInfo");
+
+router.get("/gameinfo/listgames", async (req, res, next) => {
+  try {
+    // await GameInfo.create({
+    //   title: "Mario",
+    //   gameId: "120011",
+    //   firstLoginInfo: { stars: 5, coins: 125 },
+    // });
+    const list = await GameInfo.find();
+    res.status(200).json({ message: list });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "/gameinfi/listgames not working" });
+  }
+});
 
 /* GET gameinfo route. App route starts with /gameinfo */
 router.get("/gameinfo/:gameId", (req, res, next) => {
